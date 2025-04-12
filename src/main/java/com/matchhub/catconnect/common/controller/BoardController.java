6d53a677ce8544c0b83a3b80f79794832c393f0c
tletle7102 <tletle7102@gmail.com>
2025-04-13 06:45:38 +0900

    feat(Board): Board controller에 list를 조회하기 위한 showBoardList 메서드 추가 - showBoardList: Thymeleaf 뷰에 데이터를 넘기기 위한 Model을 인자로 사용하여 board/list.html를 렌더링

--- src/main/java/com/matchhub/catconnect/common/controller/BoardController.java
+++ src/main/java/com/matchhub/catconnect/common/controller/BoardController.java
@@ -16,7 +16,6 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
-
     @GetMapping("/board/write")
     public String showWriteForm() {
         return "board/write";
@@ -31,6 +30,18 @@ public class BoardController {
         BoardEntity board = new BoardEntity(title, content, location, 1L);
         boardRepository.save(board);
         return "redirect:/home";
+    }
+
+    @GetMapping("/board/list")
+    public String showBoardList(Model model) {
+        // JPA가 제공하는 기본 메서드로 DB에서 모든 게시글 조회
+        List<BoardEntity> boards = boardRepository.findAll();
+
+        // Thymeleaf view에 데이터로 전달하기 위해, "boards"라는 키로 boards라는 변수를 속성 추
+        model.addAttribute("boards", boards);
+
+        // board/list.html 렌더링
+        return "board/list";
     }
 
 }
