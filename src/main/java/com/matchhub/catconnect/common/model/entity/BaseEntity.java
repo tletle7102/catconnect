ebebced78166c38e3ad45d1510f6947a5202ac63
tletle7102 <tletle7102@gmail.com>
2025-05-01 07:44:12 +0900

    refactor: 기획 및 설계가 변경됨에 따른 변경요소 전체 적용
    - build.gradle: 의존성 변경
    - 어플리케이션 설정 파일 변경
    - 메인화면 뷰: 상단 네비바, 중단 좌측 네비바, 하단부, 메인화면 index.html
    - 홈 메인화면 뷰 컨트롤러 생성
    - JPA Auditing & 공통 기초 Base엔티티 정의

--- src/main/java/com/matchhub/catconnect/common/model/entity/BaseEntity.java
+++ src/main/java/com/matchhub/catconnect/common/model/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.matchhub.catconnect.common.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -15,8 +16,9 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDttm;
 
     @LastModifiedDate
-    private LocalDateTime lastModifiedDttm;
+    private LocalDateTime updatedDttm;
 }
--- src/main/java/com/matchhub/catconnect/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.matchhub.catconnect.domain.user.repository;
-
-import com.matchhub.catconnect.domain.user.model.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
