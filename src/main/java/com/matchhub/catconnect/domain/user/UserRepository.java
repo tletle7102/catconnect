f65c16b84a748747faffe5587f114565481c2dd0
tletle7102 <tletle7102@gmail.com>
2025-05-03 08:39:01 +0900

    feat(user): 사용자 정보 JPA 리포지토리 생성

---
+++ src/main/java/com/matchhub/catconnect/domain/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.matchhub.catconnect.domain.user;
+
+import com.matchhub.catconnect.domain.user.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
