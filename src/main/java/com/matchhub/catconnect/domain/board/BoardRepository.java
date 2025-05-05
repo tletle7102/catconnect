f5a6c77bae5bb889a6e0bc6ef4e54fbdcf568441
tletle7102 <tletle7102@gmail.com>
2025-05-05 22:09:32 +0900

    feat(board): 게시글 정보 JPA 리포지토리 생성

---
+++ src/main/java/com/matchhub/catconnect/domain/board/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.matchhub.catconnect.domain.board;
+
+import com.matchhub.catconnect.domain.board.model.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
