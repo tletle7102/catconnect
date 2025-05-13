ac35b438bf7434b3634077d5facb20923f78cbf6
tletle7102 <tletle7102@gmail.com>
2025-05-13 22:15:56 +0900

    feat(comment): 기존 게시글 JPA 리포지토리에 게시글을 조회하는 메서드를 오버라이드하여 @EntityGraph로 연관된 속성 필드들의 컬렉션 정보들을 즉시 로딩하도록 설정

--- src/main/java/com/matchhub/catconnect/domain/board/repository/BoardRepository.java
+++ src/main/java/com/matchhub/catconnect/domain/board/repository/BoardRepository.java
@@ -1,7 +1,13 @@
 package com.matchhub.catconnect.domain.board.repository;
 
 import com.matchhub.catconnect.domain.board.model.entity.Board;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @EntityGraph(attributePaths = {"comments"})
+    Optional<Board> findById(Long id);
 }
