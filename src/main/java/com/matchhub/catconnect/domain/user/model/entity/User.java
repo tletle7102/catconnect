48e6b2f5acdee4e4baf112d16826b7af9daa4de0
tletle7102 <tletle7102@gmail.com>
2025-06-12 21:36:13 +0900

    refactor(user): 사용자에 대한 정보가 유일해지고 중복을 허용하지 않게 됨에 따라 엔티티 필드 제약조건 추가와 명시적 연관관계 설정

--- src/main/java/com/matchhub/catconnect/domain/user/model/entity/User.java
+++ src/main/java/com/matchhub/catconnect/domain/user/model/entity/User.java
@@ -16,17 +16,24 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private  String username;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
+    private String password;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
-    public User(String username, String email, Role role)
+    public User(String username, String email, String password, Role role)
     {
         this.username = username;
         this.email = email;
+        this.password = password;
         this.role = role;
     }
 }
