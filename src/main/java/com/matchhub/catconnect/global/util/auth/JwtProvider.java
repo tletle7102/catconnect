e5e0f3cb4d1c2adf731a9acb9345a120ce1c84df
tletle7102 <tletle7102@gmail.com>
2025-06-13 16:56:55 +0900

    feat(auth): JWT 토큰 생성 및 검증 유틸 클래스 JwtProvider 구현
    - JwtProvider.java: HS512 알고리즘 기반의 SecretKey를 사용하여 JWT 생성
    - generateToken: username과 role을 포함한 토큰 발급 메서드 구현
    - getUsernameFromToken, getRoleFromToken: 토큰에서 사용자 정보 추출 메서드 구현
    - validateToken: 토큰의 유효성 검증 메서드 구현

---
+++ src/main/java/com/matchhub/catconnect/global/util/auth/JwtProvider.java
@@ -0,0 +1,59 @@
+package com.matchhub.catconnect.global.util.auth;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtProvider {
+
+    private final SecretKey secretKey = Jwts.SIG.HS512.key().build();
+    private final long expirationTime = 1000 * 60 * 60; // 1시간
+
+    public String generateToken(String username, String role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationTime ))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public String getRoleFromToken(String token) {
+        return  Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload()
+                    .get("role", String.class);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
