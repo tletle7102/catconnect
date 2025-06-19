0dcd238687c069ad0badfbd4f9d38ff00efea50e
tletle7102 <tletle7102@gmail.com>
2025-06-19 21:44:03 +0900

    feat(user): 회원 생성 시 비밀번호 등록 및 암호화 로직 추가
    - UserController.java: PasswordEncoder를 주입받아 createUser 메서드에서 비밀번호 암호화 후 저장하도록 수정
    - 기존 createUser 메서드: password 파라미터 추가 및 User 생성자 변경

--- src/main/java/com/matchhub/catconnect/common/controller/UserController.java
+++ src/main/java/com/matchhub/catconnect/common/controller/UserController.java
@@ -3,6 +3,7 @@ package com.matchhub.catconnect.common.controller;
 import com.matchhub.catconnect.domain.user.repository.UserRepository;
 import com.matchhub.catconnect.domain.user.model.entity.User;
 import com.matchhub.catconnect.domain.user.model.enums.Role;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/users")
@@ -32,8 +35,10 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestParam String username, @RequestParam String email) {
-        User user = new User(username, email, Role.USER);
+    public String createUser(@RequestParam String username,
+                             @RequestParam String email,
+                             @RequestParam String password) {
+        User user = new User(username, email, passwordEncoder.encode(password),Role.USER);
         userRepository.save(user);
         return "redirect:/users";
     }
