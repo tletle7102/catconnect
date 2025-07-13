a44f633ad74ac5259f8b62597060c0f93bf8f106
tletle7102 <tletle7102@gmail.com>
2025-07-13 12:00:00 +0900

    feat(like): 특정 게시글에 대한 사용자의 좋아요 여부 확인 메서드 추가

--- src/main/java/com/matchhub/catconnect/domain/like/repository/LikeRepository.java
+++ src/main/java/com/matchhub/catconnect/domain/like/repository/LikeRepository.java
@@ -4,4 +4,5 @@ import com.matchhub.catconnect.domain.like.model.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByBoardIdAndUsername(Long boardId, String username);
 }
