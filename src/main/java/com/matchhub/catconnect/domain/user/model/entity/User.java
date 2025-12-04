466a75e5bbaf082482876d0846f5e747bb1f1ece
tletle7102 <tletle7102@gmail.com>
2025-12-04 12:00:00 +0900

    feat(validation): User 엔티티에 유효성 검사 어노테이션 추가

--- src/main/java/com/matchhub/catconnect/domain/user/model/entity/User.java
+++ src/main/java/com/matchhub/catconnect/domain/user/model/entity/User.java
@@ -2,7 +2,12 @@ package com.matchhub.catconnect.domain.user.model.entity;
 
 import com.matchhub.catconnect.common.model.entity.BaseEntity;
 import com.matchhub.catconnect.domain.user.model.enums.Role;
+import com.matchhub.catconnect.global.validation.ValidEmailDomain;
+import com.matchhub.catconnect.global.validation.ValidUsername;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,12 +21,19 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "사용자 이름은 필수 입니다.")
+    @ValidUsername
     @Column(nullable = false, unique = true)
     private String username;
 
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "유효한 이메일 형식이어야 합니다.")
+    @ValidEmailDomain
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 6, max = 100, message = "비밀번호는 6~100자여야 합니다.")
     @Column(nullable = false)
     private String password;
 
