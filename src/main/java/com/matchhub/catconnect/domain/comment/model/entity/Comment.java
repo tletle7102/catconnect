795a06fa84b50fb4fdc0daa3d2b6d3c226d21f29
tletle7102 <tletle7102@gmail.com>
2025-12-06 12:00:00 +0900

    feat(validation): Comment 엔티티에 유효성 검사 어노테이션 추가

--- src/main/java/com/matchhub/catconnect/domain/comment/model/entity/Comment.java
+++ src/main/java/com/matchhub/catconnect/domain/comment/model/entity/Comment.java
@@ -2,7 +2,10 @@ package com.matchhub.catconnect.domain.comment.model.entity;
 
 import com.matchhub.catconnect.common.model.entity.BaseEntity;
 import com.matchhub.catconnect.domain.board.model.entity.Board;
+import com.matchhub.catconnect.global.validation.RestrictedString;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,9 +19,13 @@ public class Comment extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "댓글 내용은 필수입니다.")
+    @Size(max = 500, message = "댓글은 500자 이내여야 합니다.")
+    @RestrictedString
     @Column(nullable = false)
     private String content;
 
+    @NotBlank(message = "작성자는 필수입니다.")
     @Column(nullable = false)
     private String author;
 
