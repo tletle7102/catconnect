23ffdd1a441c7c19d537335f6e79a9c00e977e23
tletle7102 <tletle7102@gmail.com>
2025-12-06 15:00:00 +0900

    feat(validation): CommentRequestDTO에 내용 필드 유효성 검사 강화
    - content 필드에 최대 길이 500자 제한(@Size) 추가
    - RestrictedString 커스텀 어노테이션 적용으로 문자열 제한 강화

--- src/main/java/com/matchhub/catconnect/domain/comment/model/dto/CommentRequestDTO.java
+++ src/main/java/com/matchhub/catconnect/domain/comment/model/dto/CommentRequestDTO.java
@@ -1,6 +1,8 @@
 package com.matchhub.catconnect.domain.comment.model.dto;
 
+import com.matchhub.catconnect.global.validation.RestrictedString;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +10,7 @@ import lombok.Setter;
 @Setter
 public class CommentRequestDTO {
     @NotBlank(message = "댓글 내용은 필수입니다.")
+    @Size(max = 500, message = "댓글은 500자 이내여야 합니다.")
+    @RestrictedString
     private String content;
 }
