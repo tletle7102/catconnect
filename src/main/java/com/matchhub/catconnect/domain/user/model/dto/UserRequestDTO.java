e38896d1e161e074f4bfeb080d7ee42e35d8662d
tletle7102 <tletle7102@gmail.com>
2025-12-06 13:00:00 +0900

    feat(validation): UserRequestDTO에 유효성 검사 어노테이션 추가
    - username에 @ValidUsername 커스텀 유효성 검사 적용
    - password에 길이 제약(@Size) 추가로 보안 강화
    - email에 도메인 검증용 @ValidEmailDomain 추가로 유효성 강화

--- src/main/java/com/matchhub/catconnect/domain/user/model/dto/UserRequestDTO.java
+++ src/main/java/com/matchhub/catconnect/domain/user/model/dto/UserRequestDTO.java
@@ -1,7 +1,10 @@
 package com.matchhub.catconnect.domain.user.model.dto;
 
+import com.matchhub.catconnect.global.validation.ValidEmailDomain;
+import com.matchhub.catconnect.global.validation.ValidUsername;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,13 +12,16 @@ import lombok.Setter;
 @Setter
 public class UserRequestDTO {
     @NotBlank(message = "사용자 이름은 필수입니다.")
+    @ValidUsername
     private String username;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 6, max = 100, message = "비밀번호는 6~100자여야 합니다.")
     private String password;
 
     @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "유효한 이메일 형식이어야 합니다.")
+    @ValidEmailDomain
     private String email;
 
 }
