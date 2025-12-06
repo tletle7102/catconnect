dac42584b42907bdd51af98694197f9727b4a99e
tletle7102 <tletle7102@gmail.com>
2025-12-06 14:00:00 +0900

    feat(validation): BoardRequestDTO 제목 필드에 유효성 검사 어노테이션 추가
    - 제목 필드에 최대 길이 100자 제한(@Size) 적용
    - 문자열 제한 조건 커스텀 어노테이션(@RestrictedString) 추가로 입력값 검증 강화

--- src/main/java/com/matchhub/catconnect/domain/board/model/dto/BoardRequestDTO.java
+++ src/main/java/com/matchhub/catconnect/domain/board/model/dto/BoardRequestDTO.java
@@ -1,6 +1,7 @@
 package com.matchhub.catconnect.domain.board.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Setter
 public class BoardRequestDTO {
     @NotBlank(message = "제목은 필수입니다.")
+    @Size(max = 100, message = "제목은 100자 이내여야 합니다.")
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
