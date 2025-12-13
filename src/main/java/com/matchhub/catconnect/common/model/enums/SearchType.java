93f9726e06b0fdc70c654da9ae20b6951202536a
tletle7102 <tletle7102@gmail.com>
2025-12-13 17:00:00 +0900

    feat(search): 검색 타입 Enum 추가
    - ALL, BOARD, COMMENT 정의
    - API 파라미터 검증 및 분기 처리용

---
+++ src/main/java/com/matchhub/catconnect/common/model/enums/SearchType.java
@@ -0,0 +1,13 @@
+package com.matchhub.catconnect.common.model.enums;
+
+/**
+ * 검색 타입을 정의하는 Enum
+ * ALL: 전체 검색 (게시글 + 댓글)
+ * BOARD: 게시글 검색
+ * COMMENT: 댓글 검색
+ */
+public enum SearchType {
+    ALL,
+    BOARD,
+    COMMENT
+}
