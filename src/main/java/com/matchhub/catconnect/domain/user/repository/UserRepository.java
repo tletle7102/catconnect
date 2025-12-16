468b1ec4c549aebf1d6e0a3720381e65c59fd57c
tletle7102 <tletle7102@gmail.com>
2025-12-16 14:00:00 +0900

    feat(pagination): UserRepository에 페이지네이션 메서드 추가
    - findAll 메서드에 Pageable 파라미터 추가
    - Page<User> 반환 타입으로 페이지네이션 지원

--- src/main/java/com/matchhub/catconnect/domain/user/repository/UserRepository.java
+++ src/main/java/com/matchhub/catconnect/domain/user/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.matchhub.catconnect.domain.user.repository;
 
 import com.matchhub.catconnect.domain.user.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Page<User> findAll(Pageable pageable);
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
