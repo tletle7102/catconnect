a24af72f57f4f468eca87419dcca4854613be177
tletle7102 <tletle7102@gmail.com>
2025-12-16 13:00:00 +0900

    feat(pagination): LikeRepository에 페이지네이션 메서드 추가
    - findAll 메서드에 Pageable 파라미터 추가
    - Page<Like> 반환 타입으로 페이지네이션 지원

--- src/main/java/com/matchhub/catconnect/domain/like/repository/LikeRepository.java
+++ src/main/java/com/matchhub/catconnect/domain/like/repository/LikeRepository.java
@@ -1,8 +1,12 @@
 package com.matchhub.catconnect.domain.like.repository;
 
 import com.matchhub.catconnect.domain.like.model.entity.Like;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    Page<Like> findAll(Pageable pageable);
     boolean existsByBoardIdAndUsername(Long boardId, String username);
 }
