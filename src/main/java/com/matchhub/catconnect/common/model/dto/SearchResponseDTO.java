09d050319dd85c9aeb52ed2d83eca80d1113e4b3
tletle7102 <tletle7102@gmail.com>
2025-12-17 16:00:00 +0900

    feat(pagination): SearchResponseDTO에 Page 필드 추가
    - boardPage, commentPage 필드 추가
    - 검색 결과 페이지네이션 응답 지원

--- src/main/java/com/matchhub/catconnect/common/model/dto/SearchResponseDTO.java
+++ src/main/java/com/matchhub/catconnect/common/model/dto/SearchResponseDTO.java
@@ -5,12 +5,14 @@ import com.matchhub.catconnect.domain.comment.model.dto.CommentResponseDTO;
 import com.matchhub.catconnect.domain.user.model.dto.UserResponseDTO;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 /**
  * 통합 검색 결과를 담는 DTO
  * 게시글, 댓글, 사용자 검색 결과를 각각의 리스트로 포함함
+ * 페이지네이션을 지원하는 버전과 리스트 버전 모두 제공함
  */
 @Getter
 @Setter
@@ -18,6 +20,8 @@ public class SearchResponseDTO {
     private List<BoardResponseDTO> boards;
     private List<CommentResponseDTO> comments;
     private List<UserResponseDTO> users;
+    private Page<BoardResponseDTO> boardPage;
+    private Page<CommentResponseDTO> commentPage;
     private String keyword;
     private String searchType;
 }
