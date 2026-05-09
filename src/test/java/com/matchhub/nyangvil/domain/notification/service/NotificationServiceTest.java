93c8a2a9189cc000aafac8e796dbe68d3928d271
tletle7102 <tletle7102@gmail.com>
2026-05-09 10:17:42 +0900

    refactor(rename): [BE] test/domain/notification 패키지 리네이밍
    - 대상 테스트 파일 1개

--- src/test/java/com/matchhub/nyangvil/domain/notification/service/NotificationServiceTest.java
+++ src/test/java/com/matchhub/nyangvil/domain/notification/service/NotificationServiceTest.java
@@ -1,6 +1,6 @@
-package com.matchhub.catconnect.domain.notification.service;
+package com.matchhub.nyangvil.domain.notification.service;
 
-import com.matchhub.catconnect.domain.notification.model.enums.NotificationChannel;
+import com.matchhub.nyangvil.domain.notification.model.enums.NotificationChannel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
