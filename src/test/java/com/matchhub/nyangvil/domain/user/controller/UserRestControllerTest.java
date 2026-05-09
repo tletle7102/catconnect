ec6ff448fd52787c77e8259981c14534e0076267
tletle7102 <tletle7102@gmail.com>
2026-05-09 11:56:21 +0900

    refactor(rename): [BE] test/domain/user 패키지 리네이밍
    - 대상 테스트 파일 2개

--- src/test/java/com/matchhub/nyangvil/domain/user/controller/UserRestControllerTest.java
+++ src/test/java/com/matchhub/nyangvil/domain/user/controller/UserRestControllerTest.java
@@ -1,10 +1,10 @@
-package com.matchhub.catconnect.domain.user.controller;
+package com.matchhub.nyangvil.domain.user.controller;
 
-import com.matchhub.catconnect.domain.user.model.dto.UserResponseDTO;
-import com.matchhub.catconnect.domain.user.model.entity.User;
-import com.matchhub.catconnect.domain.user.model.enums.Role;
-import com.matchhub.catconnect.domain.user.repository.UserRepository;
-import com.matchhub.catconnect.domain.user.service.UserService;
+import com.matchhub.nyangvil.domain.user.model.dto.UserResponseDTO;
+import com.matchhub.nyangvil.domain.user.model.entity.User;
+import com.matchhub.nyangvil.domain.user.model.enums.Role;
+import com.matchhub.nyangvil.domain.user.repository.UserRepository;
+import com.matchhub.nyangvil.domain.user.service.UserService;
 import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
